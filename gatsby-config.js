a9e14a4fa8f4e752347af5908e54ae8efdaabb5d
Arindam Basu <arin.basu@gmail.com>
2021-06-08 01:10:19 +1200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Data Science and Machine Learning Thoughts`,
+    description: `Posts and ideas on data science and machine learning`,
+    author: `@arinbasu`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
